f41293f5b2b6a15b617a59e674b030ed7696c480
viniciuspinheiroara <viniciuspinheiroara@gmail.com>
2025-03-26 08:18:43 -0300

    atualização

--- functions/consulta.py
+++ functions/consulta.py
@@ -11,7 +11,9 @@ from os import environ
 from dotenv import load_dotenv
 load_dotenv()
 
-driver_path = environ.get('CHROMEDRIVER')
+driver_version = environ.get('CHROMEDRIVER')
+driver_path = environ.get('PATHCHROMEDRIVER')
+
 
 def consulta(nomeC,cel,email):
     global url 
@@ -21,31 +23,31 @@ def consulta(nomeC,cel,email):
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--ignore-ssl-errors')
     options.add_argument('--allow-insecure-localhost')
-    # driver = webdriver.Chrome(options=options, service=Service(driver_path))
+    driver = webdriver.Chrome(options=options, service=Service(driver_path))
     # driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager(driver_version=driver_path).install()))
+    # driver = webdriver.Chrome(service=Service(ChromeDriverManager(driver_version=driver_version).install()))
     driver.get(url)
     sleep(7)
 
     driver.find_element(By.XPATH,"/html/body/div[2]/div/div[3]/button[1]").click()
-    sleep(1)
+    sleep(3)
     driver.find_element(By.XPATH,"//*[@id='Padrao2000_Nome']").send_keys(nomeC)
-    sleep(1)
+    sleep(3)
     driver.find_element(By.XPATH,"//*[@id='Padrao2000_Telefone_celMod1']").send_keys(cel)
-    sleep(1)
+    sleep(3)
     driver.find_element(By.XPATH,"//*[@id='Padrao2000_E_mail']").send_keys(email)
     sleep(3)
     driver.find_element(By.XPATH,"//*[@id='DIVPadrao2000_Modelo']/div/span/span[1]/span").click()
-    sleep(1)
+    sleep(3)
     driver.find_element(By.XPATH,"/html/body/span/span/span[1]/span/input").send_keys(modelo)
-    sleep(1)
+    sleep(3)
     driver.find_element(By.XPATH,"//*[@id='select2-Padrao2000_ModeloSelect-results']/li[1]/ul/li").click()
     sleep(3)
     driver.find_element(By.XPATH,"//*[@id='DIVPadrao2000_Anofabricacao']/div/div[2]/label[1]").click()
     sleep(10)
     driver.find_element(By.XPATH,"//*[@id='select2-Padrao2000_Cobertura8999-container']").click()
     sleep(3)
-    driver.find_element(By.XPATH,"//*[@id='select2-Padrao2000_Cobertura8999-result-bcdk-3']").click()
+    # driver.find_element(By.XPATH,"//*[@id='select2-Padrao2000_Cobertura8999-result-bcdk-3']").click()
 
 nomeC = "Vinicius De Araujo"
 cel = "85 989313544"
